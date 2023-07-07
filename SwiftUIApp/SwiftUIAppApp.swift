35e31901c392f83c3bb1c6c02e48fe8a3761fc59
Vyacheslav <slava.orlob@gmail.com>
2023-07-07 11:10:15 +0300

    Initial Commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Слава Орлов on 07.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
